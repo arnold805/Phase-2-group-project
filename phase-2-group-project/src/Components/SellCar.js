1b7c5ac520a74378f23f76bd5d72aa85195a4ee3
Greg <garnold.805@gmail.com>
2022-05-12 21:26:07 -0500

    added sell car form input and post

--- phase-2-group-project/src/Components/SellCar.js
+++ phase-2-group-project/src/Components/SellCar.js
@@ -1,11 +1,54 @@
-import React from "react";
+import React, {useState} from "react";
 
 function SellCar() {
+    const [formData, setFormData] = useState({
+        type: "",
+        make: "",
+        model: "",
+        condition: "",
+        price: "",
+        image: "",
+      });
+    
+      function handleChange(event) {
+        setFormData({
+          ...formData,
+          [event.target.name]: event.target.value,
+        })
+      }
+
+      function handleSubmit(event) {
+        // event.preventDefault();
+        fetch("http://localhost:3000/vehicles", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            type: formData.type,
+            make: formData.make,
+            model: formData.model,
+            condition: formData.condition,
+            price: formData.price,
+            image: formData.image,
+          }),
+        });
+      }
+
     return (
-        <div className="SellCar">
-            Sell Car
+        <div className="sell-car">
+          <h2>Sell Car</h2>
+          <form onSubmit={handleSubmit}>
+            <input type="text" name="type" placeholder="type" value={formData.type} onChange={handleChange} />
+            <input type="text" name="make" placeholder="make" value={formData.make} onChange={handleChange}/>
+            <input type="text" name="model"  placeholder="model" value={formData.model} onChange={handleChange}/>
+            <input type="text" name="condition"  placeholder="new or used" value={formData.condition} onChange={handleChange}/>
+            <input type="number" name="price" placeholder="price" value={formData.price} onChange={handleChange}/>
+            <input type="text" name="image"  placeholder="image URL" value={formData.image} onChange={handleChange}/>
+            <input type="submit" value="Add Car Listing"/>
+          </form>
         </div>
-    )
-}
+      );
+    }
 
 export default SellCar;
